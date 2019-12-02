b086c64f9b5f1d21e2889997ae194eb8cb2c7e87
Markus Schader <mail@markusschader.de>
2019-12-02 09:47:56 +0100

    Day1: Add test module main method

--- Day1/tests.py
+++ Day1/tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import unittest
-import fuel_calc
+
+from fuel_calc import get_required_fuel
 
 class TestCountFuel(unittest.TestCase):
 
@@ -8,4 +9,7 @@ class TestCountFuel(unittest.TestCase):
         self.assertEqual(2, get_required_fuel(12))
         self.assertEqual(2, get_required_fuel(14))
         self.assertEqual(654, get_required_fuel(1969))
-        self.assertEqual(100756, get_required_fuel(33583))
+        self.assertEqual(33583, get_required_fuel(100756))
+
+if __name__ == '__main__':
+    unittest.main()
