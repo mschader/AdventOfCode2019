9e448f94740dfa1581caadca7e6eca60b2f7534c
Markus Schader <mail@markusschader.de>
2019-12-02 10:57:34 +0100

    Day1: Refactor solve script

--- Day1/solve.py
+++ Day1/solve.py
@@ -4,18 +4,13 @@ from fuel_calc import get_required_fuel, get_required_fuel_rec
 puzzle_input = "input1.txt"
 puzzle_input_rec = "input2.txt"
 
-with open(puzzle_input, "r") as f:
-    input_list = [line.strip() for line in f]
+def read_input_to_list(input_file):
+    with open(input_file, "r") as f:
+        input_list = [line.strip() for line in f]
+    return [int(mass) for mass in input_list]
 
-fuel_list = [get_required_fuel(int(mass)) for mass in input_list]
-final_fuel = sum(num for num in fuel_list)
+final_fuel = sum(get_required_fuel(num) for num in read_input_to_list(puzzle_input))
+final_fuel_rec = sum(get_required_fuel_rec(num) for num in read_input_to_list(puzzle_input_rec))
 
 print("Final Fuel Part One: {}".format(final_fuel))
-
-with open(puzzle_input_rec, "r") as f:
-    input_list = [line.strip() for line in f]
-
-fuel_list_rec = [get_required_fuel_rec(int(mass)) for mass in input_list]
-final_fuel_rec = sum(num for num in fuel_list_rec)
-
 print("Final Fuel Part Two: {}".format(final_fuel_rec))
