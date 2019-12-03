08afb3d4a2c3e1886c42b15eb4bde29a02bdd8e8
Markus Schader <mail@markusschader.de>
2019-12-03 23:25:51 +0100

    Day3: Add tests for part1

---
+++ Day3/tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import unittest
+
+from wire_twist import closest_intersection
+
+class TestCountFuel(unittest.TestCase):
+
+    def test_examples(self):
+        self.assertEqual(6, closest_intersection("R8,U5,L5,D3", "U7,R6,D4,L4"))
+        self.assertEqual(159, closest_intersection("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"))
+        self.assertEqual(135, closest_intersection("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"))
+
+if __name__ == '__main__':
+    unittest.main()
